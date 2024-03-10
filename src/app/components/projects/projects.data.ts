7e10f8f1c0a3d7d786e8ba3f0efbf6a10c616de9
EvansSnave <127534196+EvansSnave@users.noreply.github.com>
2024-03-10 17:20:50 -0500

    create projects structure

--- src/app/components/projects/projects.data.ts
+++ src/app/components/projects/projects.data.ts
@@ -4,7 +4,10 @@ export const PROJECTS: Project[] = [
   { 
     images: ['', ''],
     title: 'Boooks',
-    technologies: [{ image_url: '', title: 'HTML'}, { image_url: '', title: 'CSS' }],
+    technologies: [
+      { image_url: '../../assets/images/html-icon.png', title: 'HTML'}, 
+      { image_url: '../../assets/images/sass-icon.png', title: 'SCSS' }
+    ],
     description: 'Its a project',
     repository_link: 'https://github.com/kiboma2021/capstone-project-front-end',
     instructions: 'Download and try',
