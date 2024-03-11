152a9dfb15ba44b7d3b0b1bce237f151590f6322
EvansSnave <127534196+EvansSnave@users.noreply.github.com>
2024-03-10 19:24:21 -0500

    create carousel

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { Project } from '../../models/project';
 import { PROJECTS } from './projects.data';
 
@@ -8,15 +8,34 @@ import { PROJECTS } from './projects.data';
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.scss']
 })
-export class ProjectsComponent implements OnInit {
+export class ProjectsComponent implements OnInit, AfterViewInit {
+
+  @ViewChild("leftBtn") leftBtn: ElementRef<HTMLButtonElement>;
+  @ViewChild("images") images: ElementRef<HTMLUListElement>;
+  @ViewChild("rightBtn") rightBtn: ElementRef<HTMLButtonElement>;
 
   projects: Project[];
 
-  constructor() {
+  constructor(private renderer: Renderer2, private elementRef: ElementRef) {
     this.projects = PROJECTS;
+    this.leftBtn = this.elementRef.nativeElement;
+    this.rightBtn = this.elementRef.nativeElement;
+    this.images = this.elementRef.nativeElement;
   }
 
   ngOnInit() {
   }
 
+  ngAfterViewInit(): void {
+    if (this.leftBtn && this.images) {
+      this.renderer.listen(this.leftBtn.nativeElement, 'click', () => {
+        this.images.nativeElement.scrollLeft -= 800;
+      });
+    }
+    if (this.rightBtn && this.images) {
+      this.renderer.listen(this.rightBtn.nativeElement, 'click', () => {
+        this.images.nativeElement.scrollLeft += 800;
+      })
+    }
+  }
 }
