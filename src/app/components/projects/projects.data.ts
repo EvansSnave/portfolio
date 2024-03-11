b0312b58d66cd86c8bbf2b5a93fd98b99fea40b6
EvansSnave <127534196+EvansSnave@users.noreply.github.com>
2024-03-10 19:47:18 -0500

    style carousel

--- src/app/components/projects/projects.data.ts
+++ src/app/components/projects/projects.data.ts
@@ -15,4 +15,19 @@ export const PROJECTS: Project[] = [
     repository_link: 'https://github.com/kiboma2021/capstone-project-front-end',
     instructions: 'Download and try',
   },
+  { 
+    images: [
+      '../../assets/images/showcase/reference2.jpg',
+      '../../assets/images/showcase/cover4.jpeg'
+    ],
+    title: 'Another project',
+    technologies: [
+      { image_url: '../../assets/images/html-icon.png', title: 'HTML'}, 
+      { image_url: '../../assets/images/sass-icon.png', title: 'SCSS' },
+      { image_url: '../../assets/images/javascript-icon.svg', title: 'Javascript' },
+    ],
+    description: 'Its a project',
+    repository_link: 'https://github.com/kiboma2021/capstone-project-front-end',
+    instructions: 'Download and try',
+  },
 ];
