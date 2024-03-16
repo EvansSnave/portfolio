4f17acfcbf0023d3959b763c2b50afdaaf8efdef
EvansSnave <127534196+EvansSnave@users.noreply.github.com>
2024-03-16 13:49:33 -0500

    test

--- src/app/services/getData.service.ts
+++ src/app/services/getData.service.ts
@@ -15,7 +15,7 @@ export class GetDataService {
 
   getItems(): Observable<Item[]> {
     return this.httpClient.get<Item[]>(`${this.apiUrl}/books`);
-  }
+  };
 
   postData(data: FormData): Observable<object> {
     return this.httpClient.post<object>(`${this.apiUrl}/books`, data).pipe(
